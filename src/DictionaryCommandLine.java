0d32611cdac653564096ee84d5a1cd4c441de69a
Ngoc Nguyen <30312571+neeooneeoon@users.noreply.github.com>
2020-09-20 00:05:02 +0700

    printing fixed

--- src/DictionaryCommandLine.java
+++ src/DictionaryCommandLine.java
@@ -7,14 +7,13 @@ public class DictionaryCommandLine {
      * @param dict the dictionary's object
      */
     public void showAllWords(Dictionary dict) {
-        int i = 1;
-        System.out.println("No | English | Vietnamese");
+        int i = 0;
+        System.out.printf("%-12s%-12s%s\n","No.","English","Vietnamese");
         for (Entry<String, String> entry : dict.wordsList.entrySet()) {
-            System.out.print(i + " ");
             i++;
             String key = entry.getKey();
             String value = entry.getValue();
-            System.out.println(key + " " + value);
+            System.out.printf("%-12s%-12s%s\n",i,key,value);
         }
     }
 
