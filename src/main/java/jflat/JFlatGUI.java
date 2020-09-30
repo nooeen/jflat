d1fac41ab393a9c01901c101482980df602c564d
Ngoc Nguyen <30312571+neeooneeoon@users.noreply.github.com>
2020-09-30 11:59:25 +0700

    javafx & maven fix

--- src/main/java/jflat/JFlatGUI.java
+++ src/main/java/jflat/JFlatGUI.java
@@ -11,7 +11,7 @@ public class JFlatGUI extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("jflat.fxml"));
-        primaryStage.setTitle("Hello World");
+        primaryStage.setTitle("JFlat");
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
     }
